7398a208c37218040671a7dacbe5f16922821f9b
Ahmad Khalili <akhalili@restaurant365.com>
2022-11-22 16:35:51 +0200

    [LK-01] Move service implementations to application layer

---
+++ backend/LastKey_Application/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using LastKey_Domain.Entities.DTOs;
+using LastKey_Domain.Interfaces;
+
+namespace LastKey_Application.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UserService(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<User> CreateUserAsync(CreateUserRequest request)
+    {
+        var userModel = _mapper.Map<LastKey_Domain.Entities.User>(request);
+        
+        userModel = userModel with
+        {
+            IsAdmin = true
+        };
+        
+
+        return _mapper.Map<User>(await _userRepository.CreateUserAsync(userModel));
+    }
+}
