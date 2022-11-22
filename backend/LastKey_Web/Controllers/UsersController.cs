cac799a54fba4551b77e8258eec6ec628e94df08
Ahmad Khalili <akhalili@restaurant365.com>
2022-11-22 16:36:48 +0200

    [LK-01] Clean up the users controller

---
+++ backend/LastKey_Web/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using LastKey_Domain.Entities.DTOs;
+using LastKey_Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LastKey_Web.Controllers;
+
+[Controller]
+[Route("api/users")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser([FromBody] CreateUserRequest request)
+    {
+        var createdUser = await _userService.CreateUserAsync(request);
+
+        return Ok(createdUser);
+    }
+}
