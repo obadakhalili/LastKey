93365e78a6c3e65f185938296b05c44f1926db6c
Ahmad Khalili <akhalili@restaurant365.com>
2022-11-22 16:36:27 +0200

    [LK-01] Move mapping profiles to presentation layer's helpers

---
+++ backend/LastKey_Web/Helpers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LastKey_Domain.Entities.DTOs;
+
+namespace LastKey_Web.Helpers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<CreateUserRequest, LastKey_Domain.Entities.User>();
+
+        CreateMap<LastKey_Domain.Entities.User, User>();
+    }
+}
