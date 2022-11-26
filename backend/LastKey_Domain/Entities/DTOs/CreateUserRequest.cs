39c3706700e67f8c144d423d24d99993e082136c
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-11-26 14:06:13 +0200

    [LK-02] Fix username naming

--- backend/LastKey_Domain/Entities/DTOs/CreateUserRequest.cs
+++ backend/LastKey_Domain/Entities/DTOs/CreateUserRequest.cs
@@ -6,7 +6,7 @@ public class CreateUserRequest
     
     public string UserImage { get; set; }
     
-    public string UserName { get; set; }
+    public string Username { get; set; }
 
     public string Password { get; set; }
 }
