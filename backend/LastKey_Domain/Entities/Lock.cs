e26397586ad7800a62dbeaf63c53adbfb85015b9
Ahmad Khalili <akhalili@restaurant365.com>
2022-11-28 15:21:48 +0200

    [LK-03] create lock model

---
+++ backend/LastKey_Domain/Entities/Lock.cs
@@ -0,0 +1,13 @@
+﻿namespace LastKey_Domain.Entities;
+
+public class Lock
+{
+    public int LockId { get; set; }
+
+    public string LockName { get; set; }
+
+    public string MacAddress { get; set; }
+
+    public int? AdminId { get; set; }
+    public User User { get; set; }
+}
