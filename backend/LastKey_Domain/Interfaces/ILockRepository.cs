1e87af3bcbeca37e7c00f90ee6399e902a958bcf
Ahmad Khalili <akhalili@restaurant365.com>
2022-11-28 20:49:41 +0200

    [LK-03] add lock update repo method

--- backend/LastKey_Domain/Interfaces/ILockRepository.cs
+++ backend/LastKey_Domain/Interfaces/ILockRepository.cs
@@ -1,4 +1,5 @@
-﻿using LastKey_Domain.Entities;
+﻿using LastKey_Domain.Entities.DTOs;
+using Lock = LastKey_Domain.Entities.Lock;
 
 namespace LastKey_Domain.Interfaces;
 
@@ -6,11 +7,13 @@ public interface ILockRepository
 {
     Task<List<Lock>> RetrieveLocksAsync();
 
-    Task AddLockAsync(Lock @lock);
+    Task AddLockAsync(int userId, Lock @lock);
 
     Task<bool> LockNameExistsForUserAsync(string lockName, int userId);
 
     Task<bool> DeleteLockForUserAsync(int userId, int lockId);
 
     Task<List<Lock>?> RetrieveLocksForUserAsync(int userId);
+
+    Task<Lock> UpdateLockNameAsync(UpdateLockRequest request);
 }
--- backend/LastKey_Infrastructure/Repositories/LockRepository.cs
+++ backend/LastKey_Infrastructure/Repositories/LockRepository.cs
@@ -1,7 +1,8 @@
-﻿using LastKey_Domain.Entities;
+﻿using LastKey_Domain.Entities.DTOs;
 using LastKey_Domain.Interfaces;
 using LastKey_Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Lock = LastKey_Domain.Entities.Lock;
 
 namespace LastKey_Infrastructure.Repositories;
 
@@ -19,23 +20,25 @@ public class LockRepository : ILockRepository
         return await _context.Locks.ToListAsync();
     }
 
-    public async Task AddLockAsync(Lock @lock)
+    public async Task AddLockAsync(int userId, Lock @lock)
     {
-        await _context.Locks.AddAsync(@lock);
+        var user = await _context.Users.FirstAsync(u => u.UserId == userId);
+        
+        user.Locks.Add(@lock);
         
         await _context.SaveChangesAsync();
     }
 
     public async Task<bool> LockNameExistsForUserAsync(string lockName, int userId)
     {
-        var user = await _context.Users.FirstAsync(u => u.UserId == userId);
+        var user = await _context.Users.Include(u => u.Locks).FirstAsync(u => u.UserId == userId);
         
         return user.Locks.Any(l => l.LockName == lockName);
     }
 
     public async Task<bool> DeleteLockForUserAsync(int userId, int lockId)
     {
-        var user = await _context.Users.FirstAsync(u => u.UserId == userId);
+        var user = await _context.Users.Include(u => u.Locks).FirstAsync(u => u.UserId == userId);
         
         var @lock = await _context.Locks.FirstAsync(l => l.LockId == lockId);
 
@@ -44,8 +47,23 @@ public class LockRepository : ILockRepository
 
     public async Task<List<Lock>?> RetrieveLocksForUserAsync(int userId)
     {
-        var user = await _context.Users.FirstAsync(u => u.UserId == userId);
+        var user = await _context.Users.Include(u => u.Locks)
+            .FirstAsync(u => u.UserId == userId);
 
         return user.Locks;
+    }
+
+    public async Task<Lock> UpdateLockNameAsync(UpdateLockRequest request)
+    {
+        var user = await _context.Users.Include(u => u.Locks)
+            .FirstAsync(u => u.UserId == request.AdminId);
+
+        var @lock = user.Locks.First(l => l.LockId == request.LockId);
+
+        @lock.LockName = request.NewName;
+
+        await _context.SaveChangesAsync();
+
+        return @lock;
     }
 }
