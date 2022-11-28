baad3cdd3b093b7409566f9ab9f6e5fccb330e75
Ahmad Khalili <akhalili@restaurant365.com>
2022-11-28 18:12:59 +0200

    [LK-03] add required responses & requests

---
+++ backend/LastKey_Domain/Entities/DTOs/LockPairRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace LastKey_Domain.Entities.DTOs;
+
+public class LockPairRequest
+{
+    public int AdminId { get; set; }
+
+    public string LockMacAddress { get; set; }
+
+    public string LockName { get; set; }
+}
---
+++ backend/LastKey_Domain/Entities/DTOs/LockUnpairRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LastKey_Domain.Entities.DTOs;
+
+public class LockUnpairRequest
+{
+    public int LockId { get; set; }
+
+    public int UserId { get; set; }
+}
