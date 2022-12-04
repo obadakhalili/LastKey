f677016424aaa9cfa9ff4ac5c2b6c77e9d5b8eb9
Obada Khalili <obada.akhalili@gmail.com>
2022-12-04 14:54:23 +0200

    [LK-03] setup capacitorjs

---
+++ client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'lastkey.netlify.app',
+  appName: 'LastKey',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
