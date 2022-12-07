9a04ee621f9795a3047f5a65cbb9050252f264df
Obada Khalili <obada.akhalili@gmail.com>
2022-12-07 09:13:35 +0200

    [LK-3] implement UI for listing locks and deleting them

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useAuth } from "@/utils/composables"
+
+const { logout } = useAuth()
+</script>
+
+<template>
+  <h1>Home</h1>
+  <v-btn @click="logout"> logout </v-btn>
+</template>
